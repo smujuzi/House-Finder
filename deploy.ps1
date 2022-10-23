50480c304d4edeaede416b387facb41823a44339
Stuart <stuartkasekende1@gmail.com>
2022-10-23 21:04:23 +0100

    working project :D

--- deploy.ps1
+++ deploy.ps1
@@ -5,7 +5,7 @@ New-Item -Path . -Name $new_fileName  -ItemType "file"
 Compress-Archive -Path C:\Users\Stuart\OneDrive\AWS\House-Finder\* -DestinationPath C:\Users\Stuart\OneDrive\AWS\House-Finder\$new_fileName -Force
 
 aws s3 cp $new_fileName s3://house-finder-bucket
-aws cloudformation update-stack --stack-name house-finder-stack --template-body file://C:/Users/Stuart/OneDrive/AWS/House-Finder/cloudFormation/lambda_cloud_formation_template.yaml --parameters ParameterKey=newFileName,ParameterValue=$new_fileName
+aws cloudformation update-stack --stack-name house-finder-stack-2 --template-body file://C:/Users/Stuart/OneDrive/AWS/House-Finder/cloudFormation/lambda_cloud_formation_template.yaml --parameters ParameterKey=newFileName,ParameterValue=$new_fileName
 Remove-Item -path C:\Users\Stuart\OneDrive\AWS\House-Finder\$new_fileName
 
 
