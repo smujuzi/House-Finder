1020219670d5f473bafa3f556bd66480f7dc9956
Stuart <stuartkasekende1@gmail.com>
2022-10-23 16:00:41 +0100

    search criteria split out + main func created

--- index.js
+++ index.js
@@ -2,47 +2,39 @@ const axios = require("axios");
 const Property = require("./property.js");
 require("dotenv/config");
 const upload = require("./write");
+const searchCriteria = require("./searchCriteria.js");
 
-const options = {
-  method: "GET",
-  url: "https://zoopla.p.rapidapi.com/properties/list",
-  params: {
-    area: "Bristol",
-    identifier: "Bristol",
-    category: "residential",
-    order_by: "age",
-    ordering: "descending",
-    page_number: "1",
-    page_size: "3",
-  },
-  headers: {
-    "X-RapidAPI-Key": process.env.RAPID_API_KEY,
-    "X-RapidAPI-Host": "zoopla.p.rapidapi.com",
-  },
-};
 
-axios
-  .request(options)
-  .then(function (response) {
-    let properties = response.data.listing;
-    for (let property of properties) {
 
-      listing = new Property.Property(
-        parseInt(property.listing_id),
-        property.title,
-        property.agent_name,
-        property.agent_phone,
-        property.location_is_approximate,
-        property.furnished_state,
-        property.property_type,
-        property.rental_prices.per_month,
-        property.available_from_display,
-        property.displayable_address,
-        property.details_url
-      );
-      upload.process(listing, parseInt(property.listing_id));
-    }
-  })
-  .catch(function (error) {
-    console.error(error);
-  });
+
+
+
+
+function findHouse() {
+  axios
+    .request(searchCriteria.options)
+    .then(function (response) {
+      let properties = response.data.listing;
+      for (let property of properties) {
+        listing = new Property.Property(
+          parseInt(property.listing_id),
+          property.title,
+          property.agent_name,
+          property.agent_phone,
+          property.location_is_approximate,
+          property.furnished_state,
+          property.property_type,
+          property.rental_prices.per_month,
+          property.available_from_display,
+          property.displayable_address,
+          property.details_url
+        );
+        upload.process(listing, parseInt(property.listing_id));
+      }
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+}
+
+//findHouse();
