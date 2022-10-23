81ea243a757a19ea405f6e93df9385a76add2004
Stuart <stuartkasekende1@gmail.com>
2022-10-23 19:22:37 +0100

    Lambda updated to use callback

--- index.js
+++ index.js
@@ -4,37 +4,43 @@ require("dotenv/config");
 const upload = require("./write");
 const searchCriteria = require("./searchCriteria.js");
 
-exports.handler = async (event, context, callback) => {
-  response = await findHouse().promise();
+exports.handler = (event, context, callback) => {
+  response = findHouse();
 
-  return response;
+  callback(null, response);
 };
 
 async function findHouse() {
-  axios
-    .request(searchCriteria.options)
-    .then(function (response) {
-      let properties = response.data.listing;
-      for (let property of properties) {
-        listing = new Property.Property(
-          parseInt(property.listing_id),
-          property.title,
-          property.agent_name,
-          property.agent_phone,
-          property.location_is_approximate,
-          property.furnished_state,
-          property.property_type,
-          property.rental_prices.per_month,
-          property.available_from_display,
-          property.displayable_address,
-          property.details_url
-        );
-        upload.process(listing, parseInt(property.listing_id));
-      }
-    })
-    .catch(function (error) {
-      console.error(error);
-    });
+  try {
+    axios
+      .request(searchCriteria.options)
+      .then(function (response) {
+        let properties = response.data.listing;
+        for (let property of properties) {
+          listing = new Property.Property(
+            parseInt(property.listing_id),
+            property.title,
+            property.agent_name,
+            property.agent_phone,
+            property.location_is_approximate,
+            property.furnished_state,
+            property.property_type,
+            property.rental_prices.per_month,
+            property.available_from_display,
+            property.displayable_address,
+            property.details_url
+          );
+          upload.process(listing, parseInt(property.listing_id));
+        }
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  } catch {
+    console.log("failed!");
+    return false;
+  }
+  return true;
 }
 
 //findHouse();
